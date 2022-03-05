c5b93bc13978945a1b26958582905ddc9246e8e9
tushardma167 <tushardma167@gmail.com>
2022-03-05 20:19:26 +0530

    Development of models for entity sessions and speakers.

--- src/main/java/com/app/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/app/conferencedemo/ConferenceDemoApplication.java
@@ -1,4 +1,4 @@
-package com.conferenceapp.conferencedemo;
+package com.app.conferencedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/app/conferencedemo/ConferenceDemoApplicationTests.java
+++ src/test/java/com/app/conferencedemo/ConferenceDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.conferenceapp.conferencedemo;
+package com.app.conferencedemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
